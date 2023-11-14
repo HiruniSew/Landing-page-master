bd0a1b8bb82946bb0815bb8606cfaa369688e99a
HiruniSew <hiruni.kahawalage@innovative-e.com>
2023-11-15 05:11:19 +0530

    added changes

--- js/index.js
+++ js/index.js
@@ -5,19 +5,19 @@ document.getElementById("getStartedBtn").addEventListener("click", function () {
   if (validateEmail(email)) {
     // Clear any previous error messages and styles
     errorMessage.textContent = "";
-    document.getElementById("emailInput").style.border = "1px solid #ccc";
+    document.getElementById("emailInput").style.border = "2px solid #ccc";
 
     // Proceed with your logic when the email is valid
     alert("Email is valid. Get started!");
   } else {
     // Display error message and change border color
     errorMessage.textContent = "Please enter a valid email address.";
-    document.getElementById("emailInput").style.border = "1px solid red";
+    document.getElementById("emailInput").style.border = "2px solid red";
   }
 });
 
 function validateEmail(email) {
-  // Email validation regex
+  // Email validation
   var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 }
